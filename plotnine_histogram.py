5e02e253af31537b3f8c4274aebf99bc6be9fbae
cshukai <cshukai@gmail.com>
2019-03-14 16:45:34 -0500

    plotnine imported to global namespace

--- plotnine_histogram.py
+++ plotnine_histogram.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import numpy as np
-import plotnine as p9
+from plotnine import *
+
 
 d=pd.read_csv("/dsa/data/all_datasets/USDA.csv")
 usda_data=d[np.isfinite(d['Calories'])]
-p9.qplot("Calories", data=usda_data, geom="histogram",binwidth=10)
+qplot("Calories", data=usda_data, geom="histogram",binwidth=10)
+qplot(data=usda_data, x="Calories",  geom="histogram",weight = "Protein", binwidth=10)
+
+ggplot(usda_data, aes(x="Calories")) + geom_histogram(binwidth=10, fill="lightblue") + ylab("Freqeuncy")
